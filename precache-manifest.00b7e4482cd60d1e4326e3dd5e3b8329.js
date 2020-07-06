f4ba6db1e2ba1e230f4b1be305ab4ddf1696e420
potS <1820346204@qq.com>
2020-07-06 22:59:59 +0800

    deploy

---
+++ precache-manifest.00b7e4482cd60d1e4326e3dd5e3b8329.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3530c5d34de13c2a106",
+    "url": "/Accounting-website/css/app.3c7b7f0d.css"
+  },
+  {
+    "revision": "f6308476efadd95f0b740d5de8785fd5",
+    "url": "/Accounting-website/index.html"
+  },
+  {
+    "revision": "c3530c5d34de13c2a106",
+    "url": "/Accounting-website/js/app.e46f5790.js"
+  },
+  {
+    "revision": "250b5fa66f7dcd4d36c6",
+    "url": "/Accounting-website/js/chunk-vendors.92ade372.js"
+  },
+  {
+    "revision": "e09939e8bdb3bc07e427e0d8dbf41c91",
+    "url": "/Accounting-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Accounting-website/robots.txt"
+  }
+]);
